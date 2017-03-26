114a1976f0c6727cc64e4f24b6733262f7972dd7
caswitch <mariefrance.kom@gmail.com>
2017-03-26 13:47:49 +0200

    C'est très la fatigue

---
+++ detecter.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+// Grumbles and exits
+void grumble (char const * msg) {
+	// Small 'if' to avoid the "Error: Success" problem
+	if (errno)
+		perror (msg);
+	else
+		fprintf(stderr, "%s\n", msg);
+
+	exit (EXIT_FAILURE);
+}
+
+// Checks for error. If there's one, grumble and exit 
+void assert (int return_value, char const * msg) {
+	if (return_value == -1)
+		grumble (msg);
+}
+
+// Converts a string to an int and errors out if not possible
+int tonumber (char const* str){
+	for (unsigned int i = 0; i < strlen(str); ++i)
+		if (str[i] < '0' || str[i] > '9')
+			grumble("String to number conversion fail");
+
+	return atoi(str);
+}
+
+int opt_t;
+int opt_i;
+int opt_l;
+bool opt_c;
+bool opt_h;
+
+int main (int argc, char * const argv[]) {
+	int option;
+	while ((option = getopt (argc, argv, ":t:i:l:ch")) != -1) {
+		switch (option) {
+			case 't':
+				opt_t = tonumber(optarg);
+				printf("t=%d\n", opt_t);
+				break;
+			case 'i':
+				opt_i = tonumber(optarg);
+				printf("i=%d\n", opt_i);
+				break;
+			case 'l':
+				opt_l = tonumber(optarg);
+				printf("l=%d\n", opt_l);
+				break;
+			case 'c':
+				opt_c = true;
+				printf("c=%d\n", opt_c);
+				break;
+			case 'h':
+				opt_h = true;
+				printf("h=%d\n", opt_h);
+				break;
+			case '?':
+				printf("Unknown option: %c\n", optopt);
+				break;
+			case ':':
+				printf("Option -%c takes an argument\n", optopt);
+			default :
+				grumble ("getopt");
+				break;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
