df60f494d87363db2150e923af453e97af03586e
caswitch <mariefrance.kom@gmail.com>
2017-03-30 17:14:11 +0200

    Voilà

--- detecter.c
+++ detecter.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
-#include <errno.h>
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
 
 
 // Grumbles and exits
-void grumble (char const * msg) {
+void grumble(char const * msg) {
 	// Small 'if' to avoid the "Error: Success" problem
 	if (errno)
-		perror (msg);
+		perror(msg);
 	else
 		fprintf(stderr, "%s\n", msg);
 
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
 // Checks for error. If there's one, grumble and exit 
-void assert (int return_value, char const * msg) {
+void assert(int return_value, char const * msg) {
 	if (return_value == -1)
-		grumble (msg);
+		grumble(msg);
 }
 
 // Converts a string to an int and errors out if not possible
-int tonumber (char const* str){
+int tonumber(char const* str) {
 	for (unsigned int i = 0; i < strlen(str); ++i)
 		if (str[i] < '0' || str[i] > '9')
 			grumble("String to number conversion fail");
@@ -32,15 +32,22 @@ int tonumber (char const* str){
 	return atoi(str);
 }
 
-int opt_t;
-int opt_i;
-int opt_l;
-bool opt_c;
-bool opt_h;
+// Prints a nice reminder of how to use the program
+void usage(char * const command) {
+	printf("%s [-t format] [-i intervalle] ", command);
+	printf("[-l limite] [-c] prog arg ... arg\n");
+}
 
-int main (int argc, char * const argv[]) {
+
+int main(int argc, char * const argv[]) {
+	int opt_t;
+	int opt_i;
+	int opt_l;
+	bool opt_c;
+	bool opt_h;
 	int option;
-	while ((option = getopt (argc, argv, ":t:i:l:ch")) != -1) {
+
+	while ((option = getopt(argc, argv, "+:t:i:l:ch")) != -1) {
 		switch (option) {
 			case 't':
 				opt_t = tonumber(optarg);
@@ -60,6 +67,7 @@ int main (int argc, char * const argv[]) {
 				break;
 			case 'h':
 				opt_h = true;
+				usage(argv[0]);
 				printf("h=%d\n", opt_h);
 				break;
 			case '?':
@@ -67,8 +75,9 @@ int main (int argc, char * const argv[]) {
 				break;
 			case ':':
 				printf("Option -%c takes an argument\n", optopt);
+				break;
 			default :
-				grumble ("getopt");
+				grumble("getopt");
 				break;
 		}
 	}
