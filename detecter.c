14fce88e67fba8bfff25fa5b7218aef5e66937b4
Wyvern67 <nicolas.argyriou95@gmail.com>
2017-03-31 17:12:21 +0200

    Pour que maf arrete de me faire chier, je vais commit

--- detecter.c
+++ detecter.c
@@ -4,7 +4,13 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <wait.h>
 
+#include "file.c"
+#define BUFF_SIZE 256
 
 // Grumbles and exits
 void grumble(char const * msg) {
@@ -24,7 +30,7 @@ void assert(int return_value, char const * msg) {
 }
 
 // Converts a string to an int and errors out if not possible
-int tonumber(char const* str) {
+int safe_atoi(char const* str) {
 	for (unsigned int i = 0; i < strlen(str); ++i)
 		if (str[i] < '0' || str[i] > '9')
 			grumble("String to number conversion fail");
@@ -38,6 +44,37 @@ void usage(char * const command) {
 	printf("[-l limite] [-c] prog arg ... arg\n");
 }
 
+int callProgram(char const *prog, char *const args[]){
+	//int status;
+	//int devNull;
+	int tube[2];
+	
+	assert(pipe(tube), "callProgram pipe");
+	
+	int pid = fork();
+	switch (pid) {
+		case 0:
+			//Case enfant
+			assert(close(tube[0]), "callProgram child close tube[0]");
+			assert(tube[1], "callProgram child write tube[1]");
+
+			assert(dup2(tube[1], 1), "callProgram child redirect stdout > tube[1]");
+
+			execvp(prog, args);
+			grumble("callProgram execlp");
+		case -1:
+			//Case erreur
+			grumble("callProgram fork");
+		default:
+			//Case parent
+			assert(close(tube[1]), "callProgram father close tube[1]");
+			assert(tube[0], "callProgram father read /tmp/tata");
+
+			return tube[0];
+	}
+}
+
+
 
 int main(int argc, char * const argv[]) {
 	int opt_t;
@@ -50,15 +87,15 @@ int main(int argc, char * const argv[]) {
 	while ((option = getopt(argc, argv, "+:t:i:l:ch")) != -1) {
 		switch (option) {
 			case 't':
-				opt_t = tonumber(optarg);
+				opt_t = safe_atoi(optarg);
 				printf("t=%d\n", opt_t);
 				break;
 			case 'i':
-				opt_i = tonumber(optarg);
+				opt_i = safe_atoi(optarg);
 				printf("i=%d\n", opt_i);
 				break;
 			case 'l':
-				opt_l = tonumber(optarg);
+				opt_l = safe_atoi(optarg);
 				printf("l=%d\n", opt_l);
 				break;
 			case 'c':
@@ -81,6 +118,17 @@ int main(int argc, char * const argv[]) {
 				break;
 		}
 	}
+
+	char *const args[] = {"ls", "-l", NULL};
+	char buf[BUFF_SIZE];
+	int bytes_read;
+
+	int fd = callProgram("ls", args);
+	
+	while ( (bytes_read = read(fd, &buf, BUFF_SIZE)) > 0)
+		assert(write(1, buf, bytes_read), "callProgram father write");
+
+	assert(close(fd), "callProgram father close tube[0]");
 
 	return EXIT_SUCCESS;
 }
