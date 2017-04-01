1b775d90fe475f19bcdda913144e3366770f9228
Wyvern67 <nicolas.argyriou95@gmail.com>
2017-04-01 23:28:53 +0200

    J'ai codé output_delta

--- detecter.c
+++ detecter.c
@@ -13,6 +13,8 @@
 
 #include "file.c"
 #define BUFF_SIZE 256
+#define READ 0
+#define WRITE 1
 
 
 // Globals
@@ -43,6 +45,9 @@ void assert(int return_value, char const * msg) {
 
 // Converts a string to an int and errors out if not possible
 int safe_atoi(char const* str) {
+	// If a user enters "test", 
+	// I don't want atoi to say "test" means 0. I want an error.
+
 	for (unsigned int i = 0; i < strlen(str); ++i)
 		if (str[i] < '0' || str[i] > '9')
 			grumble("String to number conversion fail");
@@ -71,24 +76,81 @@ void usage(char * const command) {
 	exit(EXIT_FAILURE);
 }
 
-bool str_delta(char* str, unsigned int size){
-	static char* lastString = NULL;
+///*
+typedef struct s_buff {
+	unsigned int size;
+	unsigned int readAddr;
+	unsigned int writeAddr;
+	char* mem;
+} *buffer;
+//*/
 
-	if (lastString == NULL){
-		lastString = str;
-		return true;
+buffer buff_putc(buffer b, char c){
+	if (b == NULL){
+		b = malloc(sizeof(struct s_buff));
+		b->mem = malloc(BUFF_SIZE);
+		b->size = BUFF_SIZE;
+		b->readAddr = 0;
+		b->writeAddr = 0;
 	}
 
-	for (unsigned int i = 0; i < size; ++i){
-		if (str[i] != lastString[i]){
-			return true;
-		}
+	if (b->writeAddr >= b->size){
+		b->size += BUFF_SIZE;
+		b->mem = realloc(b->mem, b->size);
 	}
-
-	return false;
+	b->mem[b->writeAddr] = c;
+	b->writeAddr += 1;
+	return b;
 }
 
-int callProgram(char const *prog, char *const args[]){
+int buff_getc(buffer b){
+	if (b == NULL)
+		return EOF;
+	if (b->readAddr >= b->size)
+		return EOF;
+
+	return b->mem[b->readAddr++];
+}
+
+bool buff_setpos(buffer b, bool mode, unsigned int pos){
+	if (b == NULL)
+		return false;
+	if (mode == WRITE)
+		b->writeAddr = pos;
+	else
+		b->readAddr = pos;
+	return true;
+}
+
+bool buff_reset(buffer b){
+	return buff_setpos(b, 0, 0) && buff_setpos(b, 1, 0);
+}
+
+bool output_delta(int fd) {
+	static buffer cache = NULL;
+	char new = '_';
+	char old = '_';
+	//unsigned int i;
+	bool retvalue = false;
+
+	FICHIER f = my_fdtof(fd, 0);
+
+	buff_reset(cache);
+	while (new != EOF || old != EOF) {
+		// Compare last output with what comes out of fd
+		// As we compare, we replace buff[i] with fd[i]
+		new = my_getc(f);
+		old = buff_getc(cache);
+		if (old != new){
+			retvalue = true;
+		}
+		cache = buff_putc(cache, new);
+	}
+
+	return retvalue;
+}
+
+int callProgram(char const *prog, char *const args[]) {
 	//int status;
 	//int devNull;
 	int tube[2];
@@ -127,6 +189,7 @@ int main(int argc, char * const argv[]) {
 	int bytes_read;
 
 	prog_name = argv[0];
+
 	while ((option = getopt(argc, argv, "+:t:i:l:ch")) != -1) {
 		switch (option) {
 			case 't':
@@ -174,9 +237,23 @@ int main(int argc, char * const argv[]) {
 		printf("%s    ", args[i]); //CHOU
 	}
 	args[rest] = NULL;
+	printf("\n");
+
+	int a = open("toto", O_RDONLY);
+	int b = open("tata", O_RDONLY);
+	int c = open("tata", O_RDONLY);
+	a = output_delta(a);
+	printf("\nDelta: %d\n", a);
+	b = output_delta(b);
+	printf("\nDelta: %d\n", b);
+	c = output_delta(c);
+	printf("\nDelta: %d\n", c);
+	
+
+	return EXIT_SUCCESS;
 
 	//char *const args[] = {"ls", "-l", NULL};
-	int fd = callProgram("ls", args);
+	int fd = callProgram(args[0], args);
 	
 	while ( (bytes_read = read(fd, &buf, BUFF_SIZE)) > 0)
 		assert(write(1, buf, bytes_read), "callProgram father write");
