1fd0cfacddb5283019c1bddf54b57ae40d261105
Wyvern67 <nicolas.argyriou95@gmail.com>
2017-04-07 21:03:27 +0200

    Ajout des .h et ca marche pas

---
+++ assert.c
@@ -0,0 +1,16 @@
+// Grumbles and exits
+void grumble(char* msg){
+	// Small 'if' to avoid the "Error: Success" problem
+	if (errno)
+		perror(msg);
+	else
+		fprintf(stderr, "%s\n", msg);
+
+	exit(EXIT_FAILURE);
+}
+
+// Checks for error. If there's one, grumble and exit 
+void assert(int return_value, char* msg){
+	if (return_value == -1)
+		grumble(msg);
+}
